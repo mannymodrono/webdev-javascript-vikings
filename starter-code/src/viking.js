12c183d61d02522694112fde084ddf70ece35f5d
Manny Modrono <21243@columbushs.com>
2019-12-03 11:21:02 -0500

    Started war

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -26,7 +26,48 @@ class Viking extends Soldier {
 }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    constructor(health, strength) {
+        super(health, strength);
+    }
+
+    receiveDamage (damage) {
+        this.health -= damage;
+        if (this.health > 0) {return `A Saxon has received ${damage} points of damage`;}
+        else if (this.health <= 0) {return `A Saxon has died in combat`;}
+    }
+}
 
 // War
-class War {}
+class War {
+    constructor(){
+        this.vikingArmy = [];
+        this.saxonArmy = [];
+    }
+
+    addViking(Viking) {
+        this.vikingArmy.push(Viking);
+    }
+    addSaxon(Saxon) {
+        this.saxonArmy.push(Saxon);
+    }
+    vikingAttack() {
+        let randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+        let randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+        randomSaxon.receiveDamage(randomViking.strength);
+        if (randomSaxon.health <= 0) this.saxonArmy.splice(this.saxonArmy.indexOf(randomSaxon), 1);
+        if (randomSaxon.health > 0) {return `A Saxon has received ${randomViking.strength} points of damage`;}
+        else if (randomSaxon.health <= 0) {return `A Saxon has died in combat`;}
+    }
+    saxonAttack() {
+        let randomSaxon = this.saxonArmy[Math.floor(Math.random() * this.saxonArmy.length)];
+        let randomViking = this.vikingArmy[Math.floor(Math.random() * this.vikingArmy.length)];
+        randomViking.receiveDamage(randomSaxon.strength);
+        if (randomViking.health <= 0) this.vikingArmy.splice(this.vikingArmy.indexOf(randomViking), 1);
+        if (randomViking.health > 0) {return `${randomViking.name} has received ${randomSaxon.strength} points of damage`;}
+        else if (randomViking.health <= 0) {return `${randomViking.name} has died in act of combat`;}
+    }
+    showStatus() {
+
+    }
+}
